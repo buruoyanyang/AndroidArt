aca1dcd5407df2b64e6802931868aa445b624219
buruoyanyang <xu18716352164@gmail.com>
2016-05-11 23:53:12 +0800

    Android启动模式和生命周期

---
+++ app/src/main/java/buruoyanyang/androidart/demo1.java
@@ -0,0 +1,13 @@
+package buruoyanyang.androidart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class demo1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo1);
+    }
+}
